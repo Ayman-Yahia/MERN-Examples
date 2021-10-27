a4e5e32909ae5bb67103f2bc19a083ca3afba450
Ayman Yahia <ayman.ya1997@gmail.com>
2021-10-27 11:57:03 +0300

    angular p3

--- angular-course/src/app/app.module.ts
+++ angular-course/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { ItemComponent } from './components/item/item.component';
-
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +16,9 @@ import { ItemComponent } from './components/item/item.component';
     ItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-course/src/app/components/item/item.component.ts
+++ angular-course/src/app/components/item/item.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
 import {Task} from '../../Task'
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css']
 })
 export class ItemComponent implements OnInit {
+  faTimes = faTimes;
   @Input() task!:Task;
+  @Output() onDeleteTask:EventEmitter<Task>=new EventEmitter();
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onDelete(task:Task){
+    this.onDeleteTask.emit(task);
+    
   }
 
 }
